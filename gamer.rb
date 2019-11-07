fef348a7e4855b784f8eb5e71806a46e0f62d25b
merkax <mishabigr@yandex.ru>
2019-11-07 14:33:55 +0300

    Add pattern for Player, Dealer

---
+++ gamer.rb
@@ -0,0 +1,18 @@
+class Gamer
+
+  attr_reader :balance
+
+  def initialize
+    @balance = 100
+  end
+
+  def skip
+ 
+  end
+
+  def add_map
+
+  end
+
+end
+
---
+++ player.rb
@@ -0,0 +1,11 @@
+class Player < Gamer
+
+  def initialize(name)
+    @name = name
+    super
+  end
+
+  def open_cards
+
+  end
+end
