76d7f09a98b9a382118bbbb2cf0e58e1a28dbec2
merkax <mishabigr@yandex.ru>
2019-11-07 14:25:14 +0300

    Add first version deck.rb

---
+++ deck.rb
@@ -0,0 +1,30 @@
+require_relative 'card'
+
+class Deck
+  attr_reader :cards
+
+  def initialize
+    @cards = []
+    create_deck
+    random_card
+  end
+  
+  private
+
+   def create_deck
+     Card::SUITS.each do |card, suit|
+      Card::VALUES.each do |name, value|
+        # p suit
+        # p '===='
+        # p  name
+        # p '===='
+        # p value
+        @cards << Card.new(suit, name, value)
+      end  
+    end
+  end
+
+  def random_card
+    @cards.shuffle!
+  end
+end
